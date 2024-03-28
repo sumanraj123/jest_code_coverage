7e0ed4d906bc7e12dde0caef24eb56629619b425
suman123 <sumanrajravi@bitcot.com>
2024-03-28 12:19:26 +0530

    code coverage percentage calculation

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,13 @@
-const sum = require("./sum")
+const sum = require("./sum");
+
+const sub = require("./sub");
 
 describe("sum", () => {
   test("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
+  });
+
+  test("subtracts 4 - 1 to equal 3", () => {
+    expect(sub(4, 1)).toBe(3);
   });
 });
