75c415bd2547ccf0ddc6de7f90d3d031b3d2fb33
suman123 <sumanrajravi@bitcot.com>
2024-03-28 16:08:45 +0530

    code coverage check changes

--- sum.test.js
+++ sum.test.js
@@ -2,12 +2,16 @@ const sum = require("./sum");
 
 const sub = require("./sub");
 
+const multiply = require("./mutiply");
+
 describe("sum", () => {
   test("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
   });
 
-  test("subtracts 4 - 1 to equal 3", () => {
-    expect(sub(4, 1)).toBe(3);
-  });
+  // test("subtracts 4 - 1 to equal 3", () => {
+  //   expect(sub(4, 1)).toBe(3);
+  // });
+
+ 
 });
