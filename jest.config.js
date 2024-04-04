c9e4d3426120c42785f926ed00e9a95f617ab14c
suman123 <sumanrajravi@bitcot.com>
2024-04-04 15:36:47 +0530

    MishaKav/jest-coverage-comment@main

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ const config = {
     },
   },
   collectCoverage: true,
-  reporters: ["default", "jest-junit"],
+  reporters: ["default", ["jest-junit", { outputDirectory: "coverage" }]],
 };
 
 module.exports = config;
