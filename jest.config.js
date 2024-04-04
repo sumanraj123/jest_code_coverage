034a9715805623be53c13a81f3bb2deaa9d36848
suman123 <sumanrajravi@bitcot.com>
2024-04-04 22:00:20 +0530

    coverage threshold removed

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 const config = {
   verbose: true,
   coverageReporters: ["clover", "json", "lcov", ["text", { skipFull: true }]],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
   collectCoverage: true,
 };
 
