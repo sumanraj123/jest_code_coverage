8af00df9f1bb340363e229bcd10359ae7d8e4006
suman123 <sumanrajravi@bitcot.com>
2024-04-05 06:24:58 +0530

    Updating the workflow not to create a new comment on every changes in the PR

--- calculate.test.js
+++ calculate.test.js
@@ -1,10 +1,10 @@
 const { sum, sub } = require("./calculate");
 
-describe("sum", () => {
+describe("Calculate", () => {
   test("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
   });
-  test.skip("adds 1 + 2 to equal 3", () => {
+  test("adds 1 + 2 to equal 3", () => {
     expect(sub(4, 1)).toBe(3);
   });
 });
