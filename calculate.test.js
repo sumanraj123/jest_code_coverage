7a5229fd2349cfa8d8cea7bedb496e116c673870
suman123 <sumanrajravi@bitcot.com>
2024-04-05 06:20:35 +0530

    skipping test cases to be recorded in PR

--- calculate.test.js
+++ calculate.test.js
@@ -4,4 +4,7 @@ describe("sum", () => {
   test("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
   });
+  test.skip("adds 1 + 2 to equal 3", () => {
+    expect(sub(4, 1)).toBe(3);
+  });
 });
